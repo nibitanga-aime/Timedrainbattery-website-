a04a5c7b3d75861b3efd4a3b2e4698bbff16c484
nibitanga-aime <nibitangaaime597@gmail.com>
2025-05-24 15:44:57 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,21 @@
-const cacheName = "battery-app-v1";
-const filesToCache = [
-  "./",
-  "./index.html",
-  "./favicon.png",
-  "./manifest.json"
-];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('time-drain-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/time_drain_icon_512.png'
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
